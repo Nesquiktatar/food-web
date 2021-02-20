3ed3691839c1c4cf40f9729a1e02c853309f97d0
Nesquiktatar <w9655884330@gmail.com>
2021-02-20 18:41:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+
 import Start from "./components/Start/Start";
 import Header from "./components/Start/Header/Header";
 import Navbar from "./components/Start/Navbar/Navbar";
 import Gifts from "./components/Gifts/Gifts";
-import {BrowserRouter, Route} from "react-router-dom";
 import Playgrounds from "./components/Playgrounds/Playgrounds";
 import Serving from "./components/Serving/Serving";
 import Reviews from "./components/Reviews/Reviews";
